1b16a8df7e3805e63c2bd5a97f75192aabcc607c
Nikola Dinevski <ndinevski5@gmail.com>
2025-01-18 21:10:39 +0100

    Add EKS renderer

--- index.ts
+++ index.ts
@@ -6,6 +6,9 @@ const LAMBDA_URL =
   "https://2xxdayczg5.execute-api.eu-west-1.amazonaws.com/default/puppeteer-renderer-lambda";
 const EC2_URL =
   "http://ec2-34-241-196-237.eu-west-1.compute.amazonaws.com/generate-pdf";
+const EKS_URL =
+  "https://2356F191A553D1F940E739D97597722B.gr7.eu-west-1.eks.amazonaws.com/generate-pdf";
+
 const template = readHtml("./data/template.ejs");
 const data = readJsonFile("./data/data.json");
 
@@ -16,10 +19,21 @@ async function generateDocuments(): Promise<any> {
       data,
     });
 
-    const ec2GeneratedResponse = await axios.post(EC2_URL, {
-      template,
-      data,
-    });
+    const ec2GeneratedResponse = await axios.post(
+      EC2_URL,
+      {
+        template,
+        data,
+      }
+    );
+
+    const eksGeneratedResponse = await axios.post(
+      EKS_URL,
+      {
+        template,
+        data,
+      },
+    );
 
     return {
       lambdaGeneratedPdf: {
@@ -32,6 +46,11 @@ async function generateDocuments(): Promise<any> {
         requestProcessedTime: ec2GeneratedResponse.data.requestProcessedTime,
         pdfGenerationTime: ec2GeneratedResponse.data.pdfGenerationTime,
       },
+      eksGeneratedPdf: {
+        pdf: eksGeneratedResponse.data.pdf,
+        requestProcessedTime: eksGeneratedResponse.data.requestProcessedTime,
+        pdfGenerationTime: eksGeneratedResponse.data.pdfGenerationTime,
+      },
     };
   } catch (error) {
     console.error("Error generating document:", error);
@@ -41,7 +60,7 @@ async function generateDocuments(): Promise<any> {
 
 async function saveDocuments() {
   try {
-    const { lambdaGeneratedPdf, ec2GeneratedPdf } = await generateDocuments();
+    const { lambdaGeneratedPdf, ec2GeneratedPdf, eksGeneratedPdf } = await generateDocuments();
     let pdfBuffer: Buffer;
 
     // EC2
@@ -71,6 +90,22 @@ async function saveDocuments() {
             lambdaGeneratedPdf.requestProcessedTime +
             "\n\tGeneration time: " +
             lambdaGeneratedPdf.pdfGenerationTime +
+            "\n"
+        );
+      }
+    });
+
+    // EKS
+    pdfBuffer = Buffer.from(eksGeneratedPdf.pdf || "", "base64");
+    fs.writeFile("results/eks-document.pdf", pdfBuffer, (err) => {
+      if (err) {
+        console.error("Error writing PDF file", err);
+      } else {
+        console.log(
+          "3. EKS - PDF file saved with name eks-document.pdf\n\tResponse time: " +
+            eksGeneratedPdf.requestProcessedTime +
+            "\n\tGeneration time: " +
+            eksGeneratedPdf.pdfGenerationTime +
             "\n"
         );
       }
